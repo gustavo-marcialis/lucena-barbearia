f3190bad34fad010a5701bfbc806f2306c4db119
gustavo-marcialis <gustavo.s.marcialis@gmail.com>
2024-05-31 22:55:25 -0300

    Configuração do site para pt-BR

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR',
+  },
 };
 
 export default nextConfig;
