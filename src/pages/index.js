dc924e106342ee1630af1904ade71a62f42d23a4
gustavo-marcialis <gustavo.s.marcialis@gmail.com>
2024-06-03 09:42:39 -0300

    Configuração de tags OG

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,16 @@ export default function Home() {
     <>
       <Head>
         <title>Lucena Barbearia</title>
+        <meta property="og:title" content="Lucena Barbearia" />
+        <meta property="og:description" content="Agendamento Online, raápido e facilitado." />
+        <meta property="og:image" content="/images/OG.png" />
+        <meta property="og:url" content="https://lucenabarbearia.vercel.app" />
+        <meta property="og:type" content="website" />
         <meta name="description" content="Barbearia Familiar, com Ambiente Climatizado" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
     </>
   );
 }
