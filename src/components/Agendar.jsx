d9d7ecce501866df08582f22271b0383ffd7f508
gustavo-marcialis <gustavo.s.marcialis@gmail.com>
2024-06-25 13:19:24 -0300

    Nova tela de agendamentos incluindo o novo profissional

--- src/components/Agendar.jsx
+++ src/components/Agendar.jsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InlineWidget as Calendly } from 'react-calendly';
-import { Image } from 'react-bootstrap';
+import { Image, Container, Row, Col } from 'react-bootstrap';
 
 function Agendar() {
+  const [selectedCalendlyUrl, setSelectedCalendlyUrl] = useState(null);
+
+  const profissionais = [
+    {
+      nome: 'Ivan',
+      foto: '/images/lucena.jpg',
+      calendlyUrl: 'https://calendly.com/lucenabarbearia013/lucena-barbearia?back=1&month=2024-06',
+    },
+    {
+      nome: 'Temoteo',
+      foto: '/images/temoteo.jpg',
+      calendlyUrl: 'https://calendly.com/temoteolucena/30min?back=1&month=2024-06',
+    },
+  ];
+
   return (
-    <main className="bg-escuro text-light text-center" id='agendar'>
-      <Image src="/images/agendamento.svg" alt="Agendamento Título" className='titulo' />
-      <h4>Faça seu agendamento em menos de um minuto; simples e rápido!</h4>
-      <Calendly url="https://calendly.com/lucenabarbearia013" />
+    <main className="bg-escuro text-light text-center" id="agendar">
+      <Image src="/images/agendamento.svg" alt="Agendamento Título" className="titulo" />
+      <h4>Faça seu agendamento em menos de um minuto; simples e rápido!</h4><br />
+      <h5>Clique no profissional desejado para ver a agenda</h5><br />
+      <Container>
+        <Row>
+          {profissionais.map((profissional, index) => (
+            <Col key={index} xs={12} md={6} className="mb-4">
+              <div onClick={() => setSelectedCalendlyUrl(profissional.calendlyUrl)} style={{ cursor: 'pointer' }}>
+                <Image
+                  src={profissional.foto}
+                  alt={profissional.nome}
+                  roundedCircle
+                  className="mb-2"
+                  style={{ width: '150px', height: '150px' }}
+                />
+                <h5>{profissional.nome}</h5>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+
+      {selectedCalendlyUrl && <Calendly url={selectedCalendlyUrl} />}
     </main>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
     <MyNavbar />
-    <br />
      <Banner />
      <Servicos />
      <Agendar />
