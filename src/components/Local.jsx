3001692a4a3fc2ea3b71854f028e9f7c807db21d
Gustavo Marcialis <gustavo.s.marcialis@gmail.com>
2025-03-26 14:53:32 -0300

    correçao da seçao de local

--- src/components/Local.jsx
+++ src/components/Local.jsx
@@ -6,18 +6,21 @@ function Local() {
     <section className="text-center" id='local'>
       <Image src="/images/local.svg" alt="Local Título" className='titulo' />
       <br />
-      <Image src="/images/mapa.png" alt="Mapa" style={{ width: '50vw', height: 'auto' }} />
+      {/* Adicionando o iframe para o Google Maps */}
+      <div style={{ width: '100%', maxWidth: '600px', margin: '0 auto', aspectRatio: '4 / 3' }}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3643.6083656212177!2d-46.5242573!3d-24.0448719!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce21aa1e89ab39%3A0x37b1c0ae3a88122e!2sBarbearia%20Lucena!5e0!3m2!1spt-BR!2sbr!4v1743010781249!5m2!1spt-BR!2sbr"
+          width="100%"
+          height="100%"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
       <h4>Rua Santo Antonio, 622, Vila Caiçara - Praia grande/SP</h4>
-      
-        <a target='_blank' rel='noreferrer' href='https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwjbroOQ24OGAxWVppUCHVmQDXEQ9Rd6BAhJEAM&url=%2Fmaps%2Fdir%2F%2FBarbearia%2BLucena%2Fdata%3D!4m6!4m5!1m1!4e2!1m2!1m1!1s0x94ce21aa1e89ab39%3A0x37b1c0ae3a88122e%3Fsa%3DX%26ved%3D1t%3A3061%26ictx%3D111&usg=AOvVaw0hlg9Qy_mdrIGt8y7ovePC&opi=89978449'>
-          <Button type='button' className='botao botao-dark'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
-          </svg>
-          &nbsp;
 
-          Ver Mapa</Button>
-      </a>
+    
     </section>
   );
 }
