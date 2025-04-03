7343e1eccc5993e740f5724fc54651c43b52a556
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2025-04-03 14:50:46 -0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Banner() {
   return (
     <section className="bg-escuro text-light text-center banner" id='home'>
-      <span className='atendimento'>ter - sab | 09h - 19h</span>
+      <span className='atendimento' aria-label="Horário de atendimento: terça a sábado, das 9 às 19 horas">
+  ter - sab | 09h - 19h
+</span>
       <h1>Lucena Barbearia</h1>
       <p>Barbearia Familiar, Com Ambiente Climatizado</p>
     </section>
