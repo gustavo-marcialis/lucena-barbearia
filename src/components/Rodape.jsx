82a2ec3c0634f8e317abf6d4171d0efd7943d53f
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2025-04-03 15:39:51 -0300

    Update Rodape.jsx

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 const Rodape = () => {
   return (
     <footer className="text-light text-center bg-escuro">
-          <h3>Desenvolvido por:</h3>
+          <p><strong>Desenvolvido Por</strong></p>
           <a href="https://calciphertech.vercel.app" target='blank'>
-            <img src="/images/logo-criador.svg" style={{ width: '20vw', height: 'auto' }}  alt="Calcipher Tech" />
+            <img src="/images/logo-criador.svg" style={{ width: '30vw', height: 'auto' }}  alt="Calcipher Tech" />
           </a>
     </footer>
   );
