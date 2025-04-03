9abe82951de8c43ca9d64c4bb6bffb6087c1ca5e
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2025-04-03 15:11:17 -0300

    Update Agendar.jsx

--- src/components/Agendar.jsx
+++ src/components/Agendar.jsx
@@ -36,8 +36,8 @@ function Agendar() {
   return (
     <main className="bg-escuro text-light text-center" id="agendar">
       <Image src="/images/agendamento.svg" alt="Agendamento Título" className="titulo" />
-      <h4>Faça seu agendamento em menos de um minuto; simples e rápido!</h4><br />
-      <h5>Clique no profissional desejado para ver a agenda</h5><br />
+      <p><strong>Faça seu agendamento em menos de um minuto; simples e rápido!</strong></p><br />
+      <p>Clique no profissional desejado para ver a agenda</p><br />
       <Container>
         <Row>
           {profissionais.map((profissional, index) => (
