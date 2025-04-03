b6d86455ab4f2c57e2d878808d09e727c86344fd
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2025-04-03 16:24:04 -0300

    Update MyNavbar.jsx

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -12,7 +12,7 @@ const MyNavbar = () => {
         <Nav>
             <Link to="agendar" smooth={true} duration={500} role="button">
   <button
-    className="botao-dark"
+    className="botao-dark mobile-only"
     aria-label="Fazer Agendamento"
   >
     Fazer Agendamento
@@ -33,7 +33,7 @@ const MyNavbar = () => {
           <Nav>
             <Link to="agendar" smooth={true} duration={500} role="button">
   <button
-    className="botao-dark mobile-only"
+    className="botao-dark"
     aria-label="Fazer Agendamento"
   >
     Fazer Agendamento
